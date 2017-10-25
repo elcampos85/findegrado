f92cbb4e86e3103724ee77798586aabfcb8bc557
elcampos85 <elcampos85@gmail.com>
2017-10-25 16:19:54 +0200

    Creación de formularios a usar

---
+++ GarTor/GarTor/Gestion.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GarTor
+{
+    partial class Gestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Gestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(643, 333);
+            this.Name = "Gestion";
+            this.Text = "Gestion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

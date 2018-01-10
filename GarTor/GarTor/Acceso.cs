1b4281f83de04a0d1fb12c2301ee5fd179b901bc
elcampos85 <elcampos85@gmail.com>
2018-01-10 21:10:28 +0100

    .

--- GarTor/GarTor/Acceso.cs
+++ GarTor/GarTor/Acceso.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,15 +23,36 @@ namespace GarTor
         private void logIn(object sender, EventArgs e)
         {
             
-            
 
-            string user = tbUser.Text;
-            string pass = tbPass.Text;
+            string user = tbUser.Text.ToString();
+            string pass = tbPass.Text.ToString();
+
+
+            try
+            {
+
+                String conexion = "Data Source =.\\SQLEXPRESS; Initial Catalog = Pasteleria; Integrated Security = true";
+
+                SqlConnection cnn = new SqlConnection(conexion);
+                cnn.Open();
+                String consulta = "select User from Acceso where User = " + user;
+
+                SqlCommand cmd = new SqlCommand(consulta, cnn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                MessageBox.Show(reader.GetValue(1).ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR CLAVE NO ENCONTRADA");
+            }
 
             if (pass == "123")
             {
                 MessageBox.Show("yeah");
             }
         }
+        
     }
 }

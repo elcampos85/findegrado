f7c784e972e2b41f320697593589539136632c8f
elcampos85 <elcampos85@gmail.com>
2018-01-18 12:50:34 +0100

    .

--- GarTor/GarTor/VentaMayor.Designer.cs
+++ GarTor/GarTor/VentaMayor.Designer.cs
@@ -28,7 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cesta = new System.Windows.Forms.DataGridView();
+            this.Borrar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.cesta)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cesta
+            // 
+            this.cesta.AllowUserToAddRows = false;
+            this.cesta.AllowUserToResizeColumns = false;
+            this.cesta.AllowUserToResizeRows = false;
+            this.cesta.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cesta.BackgroundColor = System.Drawing.Color.Green;
+            this.cesta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cesta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.cesta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Borrar,
+            this.Producto,
+            this.Unidad,
+            this.Precio});
+            this.cesta.GridColor = System.Drawing.Color.Green;
+            this.cesta.Location = new System.Drawing.Point(574, 21);
+            this.cesta.Name = "cesta";
+            this.cesta.RowHeadersVisible = false;
+            this.cesta.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.cesta.ShowCellErrors = false;
+            this.cesta.ShowCellToolTips = false;
+            this.cesta.ShowEditingIcon = false;
+            this.cesta.ShowRowErrors = false;
+            this.cesta.Size = new System.Drawing.Size(263, 439);
+            this.cesta.TabIndex = 2;
+            // 
+            // Borrar
+            // 
+            this.Borrar.HeaderText = "Borrar";
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Width = 45;
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            this.Producto.ReadOnly = true;
+            // 
+            // Unidad
+            // 
+            this.Unidad.HeaderText = "Unidad";
+            this.Unidad.Name = "Unidad";
+            this.Unidad.Width = 50;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            this.Precio.Width = 50;
             // 
             // VentaMayor
             // 
@@ -37,12 +95,20 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(838, 472);
+            this.Controls.Add(this.cesta);
             this.Name = "VentaMayor";
             this.Text = "VentaMayor";
+            ((System.ComponentModel.ISupportInitialize)(this.cesta)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView cesta;
+        private System.Windows.Forms.DataGridViewImageColumn Borrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
     }
 }

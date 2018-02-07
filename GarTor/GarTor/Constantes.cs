5fd70613306dfdd210ed6496aeaa5f03a6845930
elcampos85 <elcampos85@gmail.com>
2018-02-07 18:24:48 +0100

    -

--- GarTor/GarTor/Constantes.cs
+++ GarTor/GarTor/Constantes.cs
@@ -8,11 +8,12 @@ namespace GarTor
 {
     class Constantes
     {
-        public const string MAIN_RUTA= "C:/Program Files (x86)/GarTor";
-        public const string PRODUCTOS_RUTA = "C:/Program Files (x86)/GarTor/Productos";
-        public const string CATEGORIAS_RUTA = "C:/Program Files (x86)/GarTor/Productos/Categorias";
+        public const string MAIN_RUTA= "C:/GarTor";
+        public const string PRODUCTOS_RUTA = "C:/GarTor/Productos";
+        public const string CATEGORIAS_RUTA = "C:/GarTor/Productos/Categorias";
         public const string QUERY_CONSULTA_CATEGORIAS = "SELECT DISTINCT Categoria_Producto FROM Productos";
         public const string QUERY_CONSULTA_PRECIOVENTA = "SELECT PrecioVenta FROM PreciosVenta WHERE Cod_Producto=(" + QUERY_CONSULTA_CODPRODUCTO + ")";
         public const string QUERY_CONSULTA_CODPRODUCTO = "SELECT Cod_Producto FROM Productos WHERE Nombre_Producto=";
+        
     }
 }

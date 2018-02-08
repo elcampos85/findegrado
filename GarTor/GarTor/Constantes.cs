42d9a88aede9f6b8a4156eba9c3850eaa722f775
elcampos85 <elcampos85@gmail.com>
2018-02-08 15:52:40 +0100

    ç

--- GarTor/GarTor/Constantes.cs
+++ GarTor/GarTor/Constantes.cs
@@ -16,16 +16,23 @@ namespace GarTor
         public static DSProductosTableAdapters.ProductosTableAdapter productos_TA = new DSProductosTableAdapters.ProductosTableAdapter();
 
 
+        public const string QUERY_CONSULTA_CATEGORIAS = "SELECT DISTINCT Categoria_Producto FROM Productos";
+
+
 
         public const int COLUMNA_PRECIO = 3;
         public const int COLUMNA_UNIDADES = 2;
         public const int COLUMNA_NOMBRE = 1;
         public const int TAMANO_IMAGENES = 175;
+        public static string PESO_UD_PRODUCTO = "0";
+
+
+
         public const string MAIN_RUTA= "C:/GarTor";
         public const string PRODUCTOS_RUTA = "C:/GarTor/Productos";
         public const string CATEGORIAS_RUTA = "C:/GarTor/Productos/Categorias";
         public const string EXTENSION = ".png";
-        public const string QUERY_CONSULTA_CATEGORIAS = "SELECT DISTINCT Categoria_Producto FROM Productos";
-        public static string PESO_UD_PRODUCTO = "0";
+        
+        
     }
 }
